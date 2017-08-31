7279b6896a8d6d24972b7aa1478fc26cd4343a95
Shamir Wehbe <shamirwehbe@Shamirs-MacBook-Pro.local>
2017-08-30 22:49:24 -0400

    almost done with HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,30 @@
 $(document).ready(function() {
 
 
+var showGame = $("#visibleGame");
 
 
 
+
+
+
+
+
+
+// BUTTON ACTIONS
+// ===========================================================
 $("#startGame").on("click", function() {
   startGame();
 })
 
+$("#doneGame").on("click", function() {
+  // startGame();
+})
+
+
+
 function startGame() {
-  $()
+  $("#startGame").replaceWith(showGame);
 }
 
 
@@ -22,13 +37,13 @@ function startGame() {
 
 
 
+// Build a countdown timer.
 
+// The game ends when the time runs out.
 
+// The page will reveal the number of questions that players answer correctly and incorrectly.
 
-
-
-
-
+// Don't let the player pick more than one answer per question.
 
 
 
