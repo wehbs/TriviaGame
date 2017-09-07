009d3c0db87abeed802faa9e0172ded61a56e4e3
Shamir Wehbe <shamirwehbe@Shamirs-MacBook-Pro.local>
2017-09-07 16:29:47 -0400

    removed log

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,107 +1,106 @@
 $(document).ready(function() {
-// GLOBAL VARIABLES
-// ===========================================================
-var showGame = $("#visibleGame");
-var showEndGame = $("#visibleEndGame");
-var time = 30;
-var intervalId;
+  // GLOBAL VARIABLES
+  // ===========================================================
+  var showGame = $("#visibleGame");
+  var showEndGame = $("#visibleEndGame");
+  var time = 30;
+  var intervalId;
 
-// COUNTERS
-// ===========================================================
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
+  // COUNTERS
+  // ===========================================================
+  var correct = 0;
+  var incorrect = 0;
+  var unanswered = 0;
 
-// BUTTON ACTIONS
-// ===========================================================
-$("#startGame").on("click", function() {
-  startGame();
-})
+  // BUTTON ACTIONS
+  // ===========================================================
+  $("#startGame").on("click", function() {
+    startGame();
+  })
 
-$("#doneGame").on("click", function() {
-  doneGame();
-})
+  $("#doneGame").on("click", function() {
+    doneGame();
+  })
 
-// BEGIN GAME
-// ===========================================================
-function startGame() {
-  // Transition to questions element
-  $("#startGame").replaceWith(showGame);
-  // Start Timer
-  intervalId = setInterval(function() {
-    time--;
-    $("#timer").html("Time Remaining: " + time);
-    if (time == 0) {
-      doneGame();
+  // BEGIN GAME
+  // ===========================================================
+  function startGame() {
+    // Transition to questions element
+    $("#startGame").replaceWith(showGame);
+    // Start Timer
+    intervalId = setInterval(function() {
+      time--;
+      $("#timer").html("Time Remaining: " + time);
+      if (time == 0) {
+        doneGame();
+      }
+    }, 1000);
+  }
+
+  // END GAME
+  // ===========================================================
+  function doneGame() {
+
+    clearInterval(intervalId);
+
+    if ($("#correct1").prop("checked")) {
+      correct++;
+    } else {
+      incorrect++;
     }
-  }, 1000);
-}
 
-// END GAME
-// ===========================================================
-function doneGame() {
-
-  clearInterval(intervalId);
-
-  if ($("#correct1").prop("checked")) {
-    correct++;
-    console.log(correct);
-  } else {
-  incorrect++;
-  }
-
-  if ($("#correct2").prop("checked")) {
-    correct++;
+    if ($("#correct2").prop("checked")) {
+      correct++;
     } else {
-    incorrect++;
-  }
+      incorrect++;
+    }
 
-  if ($("#correct3").prop("checked")) {
-    correct++;
+    if ($("#correct3").prop("checked")) {
+      correct++;
     } else {
-    incorrect++;
-  }
+      incorrect++;
+    }
 
-  if ($("#correct4").prop("checked")) {
-    correct++;
+    if ($("#correct4").prop("checked")) {
+      correct++;
     } else {
-    incorrect++;
-  }
+      incorrect++;
+    }
 
-  if ($("#correct5").prop("checked")) {
-    correct++;
+    if ($("#correct5").prop("checked")) {
+      correct++;
     } else {
-    incorrect++;
-  }
+      incorrect++;
+    }
 
-  if ($("#correct6").prop("checked")) {
-    correct++;
+    if ($("#correct6").prop("checked")) {
+      correct++;
     } else {
-    incorrect++;
-  }
+      incorrect++;
+    }
 
-  if ($("#correct7").prop("checked")) {
-    correct++;
+    if ($("#correct7").prop("checked")) {
+      correct++;
     } else {
-    incorrect++;
-  }
+      incorrect++;
+    }
 
-  if ($("#correct8").prop("checked")) {
-    correct++;
+    if ($("#correct8").prop("checked")) {
+      correct++;
     } else {
-    incorrect++;
-  }
+      incorrect++;
+    }
 
-  // Transition to correct/incorrect element
-$("#visibleGame").replaceWith(showEndGame);
+    // Transition to correct/incorrect element
+    $("#visibleGame").replaceWith(showEndGame);
 
     $("#correct").html(correct);
     $("#incorrect").html(incorrect);
     $("#unanswered").html(unanswered);
-}
+  }
 
 
-// TODO: The page will reveal the number of questions that players answer correctly and incorrectly.
+  // TODO: The page will reveal the number of questions that players answer correctly and incorrectly.
 
 
 });
