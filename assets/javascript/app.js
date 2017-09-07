7ac82a1285e516fa5eeff81e3b53197c0f544fa6
Shamir Wehbe <shamirwehbe@Shamirs-MacBook-Pro.local>
2017-09-07 16:26:56 -0400

    added timer & correct incorrect logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
-
-
+// GLOBAL VARIABLES
+// ===========================================================
 var showGame = $("#visibleGame");
+var showEndGame = $("#visibleEndGame");
+var time = 30;
+var intervalId;
 
-
-
-
-
-
-
-
+// COUNTERS
+// ===========================================================
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
 
 // BUTTON ACTIONS
 // ===========================================================
@@ -18,49 +19,89 @@ $("#startGame").on("click", function() {
 })
 
 $("#doneGame").on("click", function() {
-  // startGame();
+  doneGame();
 })
 
-
-
+// BEGIN GAME
+// ===========================================================
 function startGame() {
+  // Transition to questions element
   $("#startGame").replaceWith(showGame);
+  // Start Timer
+  intervalId = setInterval(function() {
+    time--;
+    $("#timer").html("Time Remaining: " + time);
+    if (time == 0) {
+      doneGame();
+    }
+  }, 1000);
+}
+
+// END GAME
+// ===========================================================
+function doneGame() {
+
+  clearInterval(intervalId);
+
+  if ($("#correct1").prop("checked")) {
+    correct++;
+    console.log(correct);
+  } else {
+  incorrect++;
+  }
+
+  if ($("#correct2").prop("checked")) {
+    correct++;
+    } else {
+    incorrect++;
+  }
+
+  if ($("#correct3").prop("checked")) {
+    correct++;
+    } else {
+    incorrect++;
+  }
+
+  if ($("#correct4").prop("checked")) {
+    correct++;
+    } else {
+    incorrect++;
+  }
+
+  if ($("#correct5").prop("checked")) {
+    correct++;
+    } else {
+    incorrect++;
+  }
+
+  if ($("#correct6").prop("checked")) {
+    correct++;
+    } else {
+    incorrect++;
+  }
+
+  if ($("#correct7").prop("checked")) {
+    correct++;
+    } else {
+    incorrect++;
+  }
+
+  if ($("#correct8").prop("checked")) {
+    correct++;
+    } else {
+    incorrect++;
+  }
+
+  // Transition to correct/incorrect element
+$("#visibleGame").replaceWith(showEndGame);
+
+    $("#correct").html(correct);
+    $("#incorrect").html(incorrect);
+    $("#unanswered").html(unanswered);
 }
 
 
-
-
-
-
-
-
-
-
-
-// Build a countdown timer.
-
-// The game ends when the time runs out.
-
-// The page will reveal the number of questions that players answer correctly and incorrectly.
-
-// Don't let the player pick more than one answer per question.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// TODO: The page will reveal the number of questions that players answer correctly and incorrectly.
 
 
 });
